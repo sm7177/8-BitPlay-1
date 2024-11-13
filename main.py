2a264c0972de03b500dea322d00579e52fb8f10c
sm7177 <subrinamaharjan2020@gmail.com>
2024-11-13 18:48:16 +0545

    ..

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-def numofBits(n):
-    ones=0
-    zeroes=0
+# def numofBits(n):
+#     ones=0
+#     zeroes=0
     
-    while(n):
-        if(n & 1):
-            ones+=1
-        else:
-            zeroes+=1
-        n=n>>1
+#     while(n):
+#         if(n & 1):
+#             ones+=1
+#         else:
+#             zeroes+=1
+#         n=n>>1
         
-    print("Number of ones=",ones, "Number of zeros=",zeroes)
+#     print("Number of ones=",ones, "Number of zeros=",zeroes)
     
+# number=int(input("Enter a number:"))
+    
+# numofBits(number)
+
+
+
+
+
+
+def setornot(number,n):
+    mask=1
+    if (n & mask)==1 or (n & mask)==0:
+        if number & (1<<(n-1)):
+            print("SET")
+        else:
+            print("NOT SET")
 number=int(input("Enter a number:"))
-    
-numofBits(number)
+n=int(input("Enter the position:"))
+setornot(number,n)
